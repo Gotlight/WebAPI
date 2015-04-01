2217b3e0120496a5bda45c27ed8d43a812372aaa
Gotlight <gotlight.a@gmail.com>
2015-04-01 02:50:19 +0200

    Added startup.cs

--- WebApplication2/Startup.cs
+++ WebApplication2/Startup.cs
@@ -1,18 +1,73 @@
-﻿using System.Web.Http;
+﻿using System;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.Owin;
+using Microsoft.Owin.Security.OAuth;
 using Owin;
+using WebApplication2.Models;
 
 [assembly: OwinStartupAttribute(typeof(WebApplication2.Startup))]
 namespace WebApplication2
 {
-    public partial class Startup
+    public class Startup
     {
+
+        
+
         public void Configuration(IAppBuilder app)
         {
-            ConfigureAuth(app);
-            var config = new HttpConfiguration();
-            WebApiConfig.Register(config);
-            app.UseWebApi(config);
+            ConfigureOAuth(app);
+            //Rest of code is here;
+        }
+
+        public void ConfigureOAuth(IAppBuilder app)
+        {
+            OAuthAuthorizationServerOptions OAuthServerOptions = new OAuthAuthorizationServerOptions()
+            {
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(1),
+                Provider = new SimpleAuthorizationServerProvider()
+            };
+
+            // Token Generation
+            app.UseOAuthAuthorizationServer(OAuthServerOptions);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+
+        }
+
+        public class SimpleAuthorizationServerProvider : OAuthAuthorizationServerProvider
+        {
+            private RESTAURANTEntities2 db = new RESTAURANTEntities2();
+            public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+            {
+                context.Validated();
+            }
+
+            public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+            {
+
+                context.OwinContext.Response.Headers.Add("Access-Control-Allow-Origin", new[] { "*" });
+
+
+                var user = await db.AspNetUsers.FindAsync(context.UserName, context.Password);
+
+                    if (user == null)
+                    {
+                        context.SetError("invalid_grant", "The user name or password is incorrect.");
+                        return;
+                    }
+                
+
+                var identity = new ClaimsIdentity(context.Options.AuthenticationType);
+                identity.AddClaim(new Claim("sub", context.UserName));
+                identity.AddClaim(new Claim("role", "user"));
+
+                context.Validated(identity);
+
+            }
         }
     }
 }
